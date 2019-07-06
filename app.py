cb326f52d50f177973e657e6d3da74878bbfe293
Devang J <devang.j1998@gmail.com>
2019-07-07 00:14:28 +0530

    runnable app

---
+++ app.py
@@ -0,0 +1,111 @@
+from flask import Flask, render_template, request, url_for, abort, session
+from flask_mail import Mail, Message
+from db import *
+
+app = Flask(__name__)
+app.secret_key = ''
+csrf = CSRFProtect(app)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'vinaykatare456@gmail.com'
+app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app)
+
+@app.route('/request', methods=['POST'])
+def ask_request():
+    if request.method == "POST":
+        name = request.form["name"] 
+        to_email = request.form["email"]
+        if register_form(request.form["name"], request.form["email"], request.form["phone"], request.form["company"], request.form["company_name"], request.form["no_of_playgrounds"], request.form["location"], request.form["funding"], request.form["budget"], request.form["start_time"], request.form["preferences"]):
+            msg = Message('Ant Hill Creations welcomes you!', sender = 'vinaykatare456@gmail.com', recipients = [to_email])
+            link = "http://127.0.0.1:5000/survey.html"
+            msg.body = "Hello "+name+" \n Ant Hill Creations welcomes you! Our motto is to aim at interactive learning environments in public spaces with a primary focus on sustainability. \n Thank you for registrating with us! \n To proceed with us, fill the survey in the survey link - " + link+ "\n Regards,\n Ant Hill Creations \n https://anthillcreations.org/"
+            mail.send(msg)
+            return redirect(url_for('confirm_for_survey'))
+        abort(500)
+    return render_template('register.html')
+
+
+@app.route('/do_survey')
+def confirm_for_survey():
+   return render_template('confiem.html')
+
+
+@app.route("/survey", methods=['POST'])
+def survey():
+    if request.method == "POST":
+        to_email = request.form["email"]
+        if survey_form(request.form["project_name"], request.form["field_type"], request.form["google_location"], request.form["address"], request.form["no_of_students"], request.form["min_age"], request.form["max_age"], request.form["snake_prone"], request.form["public_location"], request.form["vandalism_prone"], request.form["soil_condition"], request.form["play_elements"], request.form["underground_connections"], request.form["electric_posts"], request.form["trees"], request.form["rocks"], request.form["water_logging"], request.form["highway"], request.form["waterbodies"], request.form["disability"], request.form["maintainance_required"], request.form["additional_requirements"], request.form["email"]
+):
+            msg = Message('Survey form received and generic proposal attached.', sender = 'vinaykatare456@gmail.com', recipients = [email_id])   
+            msg.body = "Hello, we have received your survey form and accordingly we have sent a generic proposal. Please find the attached generic proposal."
+            with app.open_resource("proposal.ppt") as fp:
+                msg.attach("proposal.ppt", "proposal.ppt", fp.read())
+            mail.send(msg)
+            return redirect(url_for('survey_done'))
+        abort(500)
+    return render_template('survey.html')
+
+
+@app.route('/surveydone')
+def survey_done():
+   return render_template('survey_done.html')
+
+
+@app.route('/dashboard/admin')
+def dashboard_admin():
+    if session.get('login'):
+        stream=request_list()
+        return render_template('admindash.html', stream=stream)
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/dashboard/admin/<user_id>')
+def dashboard_admin_user_info(user_id):
+    if session.get('login'):
+        user=request_list_by_id(user_id)
+        return render_template('projectforadmin.html', user=user, image=image)
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/dashboard/<user_id>')
+def dashboard_user(user_id):
+    if session.get('login'):
+        user = request_by_id(user_id)
+        return render_template('userdash.html', user=user)
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+        if email == "admin@admin.com" and check_password_hash("admin", password):
+            return redirect(url_for('dashboard_admin'))    
+        result = login_user(email, password)
+        if result:
+            return redirect(url_for('dashboard_user', user_id=result[0]))    
+    return render_template('login.html')
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html'), 500
+
+
+@app.errorhandler(404)
+def server_error(error):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+  app.run(host='127.0.0.1', port=8000, debug=True)
+ 
---
+++ db.py
@@ -0,0 +1,182 @@
+import psycopg2
+import os
+from flask_bcrypt import generate_password_hash, check_password_hash
+import mysql.connector
+
+# for mysql
+def connectDB(host='localhost', database='anthill', user='root', password='1234'):
+    return mysql.connector.connect(host=host, database=database, user=user, password=password)
+
+
+# for heroku postgresql
+# DATABASE_URL = os.environ['DATABASE_URL']
+# def connectDB():
+#     return psycopg2.connect(DATABASE_URL, sslmode='require')
+
+
+# Disconnect From Database
+def disconnectDB(conn):
+    conn.close()
+
+
+# Execute A Query
+def executeDB(conn, sql, values):
+    cursor = conn.cursor()
+    cursor.execute(sql, values)
+    conn.commit()
+    return cursor.lastrowid
+
+
+# Query The Database
+def queryDB(conn, sql):
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    try:
+        rows = cursor.fetchall()
+    except:
+        rows = []
+    cursor.close()
+    return rows
+
+
+# Forms and Users
+# Request Form
+def request_form(name, email, phone, company, company_name, no_of_playgrounds, location, funding, budget, start_time, preferences):
+    c = connectDB()
+    executeDB(c, "insert into user(user_id, name, email, phone, company, company_name, no_of_playgrounds, location, funding, budget, preferences) values(default, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (name, email, phone, company, company_name, no_of_playgrounds, location, funding, budget, start_time, preferences))
+    disconnectDB(c)
+    return True
+
+
+# Survey Form
+def survey_form(project_name, field_type, google_location, address, no_of_students, min_age, max_age, snake_prone, public_location, vandalism_prone, soil_condition, play_elements, underground_connections, electric_posts, trees, rocks, water_logging, highway, waterbodies, disability, maintainance_required, additional_requirements, email):
+    c = connectDB()
+    executeDB(c, "update user set project_name=%s, field_type=%s, google_location=%s, address=%s, no_of_students=%s, min_age=%s, max_age=%s, snake_prone=%s, public_location=%s, vandalism_prone=%s, soil_condition=%s, play_elements=%s, underground_connections=%s, electric_posts=%s, trees=%s, rocks=%s, water_logging=%s, highway=%s, waterbodies=%s, disability=%s, maintainance_required=%s, additional_requirements=%s where email=%s", (project_name, field_type, google_location, address, no_of_students, min_age, max_age, snake_prone, public_location, vandalism_prone, soil_condition, play_elements, underground_connections, electric_posts, trees, rocks, water_logging, highway, waterbodies, disability, maintainance_required, additional_requirements, email))
+    disconnectDB(c)
+    return True
+
+
+
+# Update User/ Basically Create account
+def create_user(email, password):
+    c = connectDB()
+    user_id = str(user_id)
+    password = generate_password_hash(password)
+    password = str(password, "utf-8")
+    executeDB(c, "update user set password=%s, status=ongoing where email=%s", (password, email))
+    disconnectDB(c)
+    return True
+
+
+# Login User
+def login_user(email, password):
+    c = connectDB()
+    result = queryDB(c, "select * from user where email=%s'", (email, ))
+    disconnectDB(c)
+    if result:
+        if check_password_hash(result[0][2], password):
+            return result[0]
+        else:
+            return False
+    else:
+        return False
+
+
+# Fetching Stuff
+# Fetch single Request by User Id
+def request_by_id(user_id):
+    c = connectDB()
+    user_id = str(user_id)
+    result = queryDB(c, "select * from user where user_id=%s", (user_id, ))
+    disconnectDB(c)
+    return result
+
+
+# Fetch Single Request by Email
+def request_by_email(email):
+    c = connectDB()
+    result = queryDB(c, "select * from user where email=%s", (email, ))
+    disconnectDB(c)
+    return result
+
+
+# Fetch all Requests
+def request_list():
+    c = connectDB()
+    result = queryDB(c, "select * from user")
+    disconnectDB(c)
+    return result
+
+
+# Fetch Image
+def image_list_by_id(user_id):
+    c = connectDB()
+    user_id = str(user_id)
+    result = queryDB(c, "select * from image where user_id=%s", (user_id, ))
+    disconnectDB(c)
+    return result
+
+
+# Fetch Remarks
+def remarks_by_id(user_id):
+    c = connectDB()
+    user_id = str(user_id)
+    result = queryDB(c, "select * from remark where user_id=%s", (user_id, ))
+    disconnectDB(c)
+    return result
+
+
+# Fetch Elements
+def elements_by_id(user_id):
+    c = connectDB()
+    user_id = str(user_id)
+    result = queryDB(c, "select * from element where user_id=%s", (user_id, ))
+    disconnectDB(c)
+    return result
+
+
+# Fetch Has Elements
+def has_elements_by_id(user_id):
+    c = connectDB()
+    user_id = str(user_id)
+    result = queryDB(c, "select * from has_element where user_id=%s", (user_id, ))
+    disconnectDB(c)
+    return result
+
+
+# Adding Stuff
+# Add element
+def add_element(element_name):
+    c = connectDB()
+    executeDB(c, "insert into element(element_id, element_name) values(default, %s)", (element_name, ))
+    disconnectDB(c)
+    return True
+
+
+# Add image
+def add_image(image_url, u_id):
+    c = connectDB()
+    u_id = str(u_id)
+    executeDB(c, "insert into image(image_id, image_url, u_id) values(default, %s, %s)", (image_url, u_id))
+    disconnectDB(c)
+    return True
+
+
+# Add remark
+def add_remark(remarks, u_id):
+    c = connectDB()
+    u_id = str(u_id)
+    executeDB(c, "insert into remark(remarks_id, remarks, u_id) values(default, %s, %s)", (remarks, u_id))
+    disconnectDB(c)
+    return True
+
+
+# Add has_element
+def add_has_element(e_id, u_id, quantity):
+    c = connectDB()
+    e_id = str(e_id)
+    u_id = str(u_id)
+    quantity = str(quantity)
+    executeDB(c, "insert into has_element(e_id, u_id, quantity) values(%s, %s, %s)", (e_id, u_id, quantity))
+    disconnectDB(c)
+    return True
